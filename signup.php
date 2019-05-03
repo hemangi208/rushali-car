e10b447a67cac45769b9dd009b6e5e960a296906
Hemangi Thakkar <hemangithakkar298@gmail.com>
2019-05-03 15:24:03 +0530

    Update signup.php
    changes in signup file

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Sonko Car</title>
+	<title>Car </title>
 	<meta charset="utf-8">
 	<meta name="author" content="pixelhint.com">
 	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
